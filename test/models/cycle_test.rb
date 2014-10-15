acbb8bcb18ed7f20df915a590fdd7c31d1e58030
Michael <Michael@MichaelHumiston.com>
2014-10-14 21:47:38 -0400

    Completed cyce unit tests

--- test/models/cycle_test.rb
+++ test/models/cycle_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class CycleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def valid_params 
+    {number: 1, user_id: 1}
+  end
+  
+    test "all members" do
+    cycle = Cycle.new valid_params
+    assert_respond_to cycle, :number
+    assert_respond_to cycle, :user
+  end
+  
+  test "model is valid" do
+    cycle = Cycle.new valid_params
+    assert cycle.valid?, "#{cycle.errors.messages}"
+  end 
+  
+  test "association" do
+    user = User.new({first_name: "Example", email: "user@example.com",
+     password: "foobar", password_confirmation: "foobar"})
+    user.save
+    3.times{ user.cycles.create(number: 1)}
+    assert_equal 3, user.cycles.size
+  end 
 end
